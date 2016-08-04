068132c103d1df583123b34579d5cc9e3f2f740b
Comet 314 <comet314@github.com>
2016-08-04 13:07:19 +0200

    Added help message to consolte example.

--- interactive-console-example.hs
+++ interactive-console-example.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE MultiWayIf #-}
+
 import Maxima
  
 maximaPrompt srv = do
     putStr "> "
     question <- getLine
-    if question == ":q"
-       then return ()
-       else do answer <- askMaxima srv question
-               print answer
-               maximaPrompt srv
+    if
+      | question == ":q" -> return ()
+      | question == ":h" ->
+        do putStrLn "Type maxima command or :q to quit."
+           maximaPrompt srv
+      | otherwise -> do answer <- askMaxima srv question
+                        print answer
+                        maximaPrompt srv
                
 main = runMaxima 4424 maximaPrompt
